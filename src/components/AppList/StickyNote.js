96b6904fac33a4f07cb30cc286fabded7539f34c
rryando <ryan.rynaldo@bukalapak.com>
2020-08-04 10:42:13 +0700

    add draggable dialog to sticky note window

--- src/components/AppList/StickyNote.js
+++ src/components/AppList/StickyNote.js
@@ -1,18 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { gsap } from 'gsap';
+import { Draggable } from "gsap/Draggable";
+import { CloseOutlined } from '@ant-design/icons';
 
 const StickyNote = () => {
   const [noteList] = useState([
     {
       id: 0,
-      text: 'noteListExample'
+      text: 'noteListExample',
+      title: 'noteListTitle',
+      isActive: true,
     }
   ])
 
+  useEffect(() => {
+    gsap.registerPlugin(Draggable);
+  }, []);
+
+  useEffect(() => {
+    Draggable.create(".sticky-note-dialog-active", {
+      trigger: ".sticky-note-toolbar-active",
+      type:"x,y",
+    });
+  }, [noteList]);
+
   return ( 
-    <div className="fixed sticky-note-dialog">
+    <div>
       { noteList.map((note, index) => {
         return (
-          <p key={index}>{note.text}</p>
+          <div className={`fixed sticky-note-dialog ${note.isActive ? 'sticky-note-dialog-active': ''}`} key={index}>
+            <div className={`flex px-1 sticky-note-toolbar ${note.isActive ? 'sticky-note-toolbar-active': ''}`}>
+              <div className="flex-1 text-left">
+                <CloseOutlined className="text-center align-middle" />
+              </div>
+              <h3 className="flex-1 text-center align-middle">{note.title}</h3>
+              <div className="flex-1"></div>
+            </div>
+            <div className="sticky-note-dialog-content p-2">
+              <p>{note.text}</p>
+            </div>
+          </div>
         )
       })}
     </div>
